f1c977cddb052478de0d361ba3192831ac3d1954
Kazuhiro Suzuki <ksauzzmsg@gmail.com>
2018-08-14 20:28:57 +0900

    python: bump version

--- lib/py/setup.py
+++ lib/py/setup.py
@@ -87,7 +87,7 @@ def run_setup(with_binary):
     twisted_deps = ['twisted']
 
     setup(name='thrift',
-          version='0.11.0',
+          version='0.11.0.1',
           description='Python bindings for the Apache Thrift RPC system',
           author='Thrift Developers',
           author_email='dev@thrift.apache.org',
